f7d3e5397882c6d4c72abbab19ef01b3f0b26b18
Hardikgiri Gosai <hardik.gosai007@gmail.com>
2023-12-18 11:02:58 +0530

    Add project files.

---
+++ Getri_ViewModels/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace Getri_ViewModels.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+
+        public string EmpName { get; set; }
+
+        public string EmpGender { get; set; }
+
+        public string EmpDept { get; set; }
+
+        public float EmpSal { get; set; }
+
+        public int AddressId { get; set; }
+    }
+}
---
+++ Getri_ViewModels/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Getri_ViewModels.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
