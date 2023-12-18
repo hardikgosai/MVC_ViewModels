c230a837af04f67cfb4d8d81666d9ee29067a034
Hardikgiri Gosai <hardik.gosai007@gmail.com>
2023-12-18 15:04:22 +0530

    Dynamic code in view

---
+++ Getri_ViewModels/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace Getri_ViewModels.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Contact { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
